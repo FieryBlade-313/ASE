883f8aa529520d6ed72790e4ad021ec3a75cb230
Sathyanarayanan R <44164854+rsathya4802@users.noreply.github.com>
2020-07-20 18:42:24 +0530

    Update search_nearest_location_through_sms.py

--- SMS/search_nearest_location_through_sms.py
+++ SMS/search_nearest_location_through_sms.py
@@ -3,15 +3,24 @@ import mysql.connector as mysql
 import urllib.request
 import urllib.parse
 import json
-import datetime
+import urllib.request
+import urllib.parse
 from bisect import bisect_left
+from ret_pass import return_pass
+
+
+def read_and_send(mess,number):
+
+    resp, code = sendSMS('17dQ9VIqzsY-SaO7zL5u6LLm30oEhjNFLtsc4GssEa', number,
+                         mess)
+    print(resp)
 
 
 # Function to search for a particular location in database
 def bin_search(arr,left,right,val):
-    while left<=right:
+    while left<= right:
         mid = int((left+right)/2)
-        if arr[mid]==val:
+        if arr[mid]== val:
             return mid
         else:
             if arr[mid] > val:
@@ -22,52 +31,84 @@ def bin_search(arr,left,right,val):
 
 
 # Function to get nearest jobs
-def get_nearest_jobs(val):
+def get_nearest_jobs(username,val,new_number):
     db = mysql.connect(
         host='localhost',
         user='root',
-        passwd='',
+        passwd=return_pass(),
         database='aseproject',
     )
     cursor = db.cursor()
-    table_name = 'jobs_available'
-    query = "SELECT * FROM jobs_available"
+    table_name = 'user_details'
+    query = "SELECT * FROM {}".format(table_name)
     cursor.execute(query)
     result = cursor.fetchall()
     items = [dict(zip([key[0] for key in cursor.description],row)) for row in result]
-    items = sorted(items,key= lambda x: x['location'])
-    print(' ')
-    location_list = [item['location'] for item in items]
-    print(' ')
-    left_index = bisect_left(location_list,val)
-    if abs(val-location_list[left_index-1]) < abs(val-location_list[left_index]):
-        left_index= left_index-1
-    right_index = left_index
-    while  right_index+1 < len(location_list) and location_list[right_index+1]==val:
-        right_index = right_index+1
-    if (right_index-left_index+1) % 2 ==0:
-        middle_index = int((left_index+right_index)/2)+1
+    present = False
+
+    f = open('message.txt','r+')
+    f.seek(0)
+    f.truncate()
+
+    for item in items:
+        if item['Username']==username:
+            number = item['mobile']
+            present=True
+            break
+    if not present:
+        print('You are not registered !!')
+        f.write('You are not registered !!'+'\n')
     else:
-        middle_index = int((left_index+right_index)/2)
-    lim = 2
-    list_ranges = [x for x in range(middle_index-lim,middle_index+lim) if 0 <= x < len(location_list)]
-    final_dict = [items[i] for i in list_ranges]
-    print('Jobs available near location {}:'.format(val))
-    for item in final_dict:
-        item['time_of_completion']= item['time_of_completion'].strftime("%B %d, %Y")
-        print("Job Name: {}".format(item['job_name']))
-        print("Base Pay: {}".format(item['base_pay']))
-        print("Time of Completion: {}".format(item['time_of_completion']))
-        print("Location: {}".format(item['location']))
+        new_number = '91'+number
+        query = "SELECT * FROM location_table"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        items = [dict(zip([key[0] for key in cursor.description],row)) for row in result]
+        items = sorted(items,key= lambda x: x['location'])
+        location_list = [item['location'] for item in items]
+        left_index = bisect_left(location_list,val)
+        if abs(val-location_list[left_index-1]) < abs(val-location_list[left_index]):
+            left_index= left_index-1
+        right_index = left_index
+        while  right_index+1 < len(location_list) and location_list[right_index+1]==val:
+            right_index = right_index+1
+        if (right_index-left_index+1) % 2 ==0:
+            middle_index = int((left_index+right_index)/2)+1
+        else:
+            middle_index = int((left_index+right_index)/2)
+        lim = 2
+        list_ranges = [x for x in range(middle_index-lim,middle_index+lim) if 0 <= x < len(location_list)]
+        final_dict = [items[i] for i in list_ranges]
+        out_str = 'Jobs available near location {}:'.format(val)
+        print(out_str)
+        f.write(out_str+'\n')
+        for item in final_dict:
+            item['time_of_completion']= item['time_of_completion'].strftime("%B %d, %Y")
+            out_str = "Job Name: {}".format(item['job_name'])
+            print(out_str)
+            f.write(out_str+'\n')
+            out_str = "Base Pay: {}".format(item['base_pay'])
+            print(out_str)
+            f.write(out_str+'\n')
+            out_str = "Time of Completion: {}".format(item['time_of_completion'])
+            print(out_str)
+            f.write(out_str+'\n')
+            out_str = "Location: {}".format(item['location'])
+            print(out_str)
+            f.write(out_str+'\n')
+            print(' ')
+        json_data = {
+                'items': final_dict,
+            }
         print(' ')
-    json_data = {
-            'items': final_dict,
-        }
-    print(' ')
-    # print(json_data)
-    json_data = json.dumps(json_data)
-    # print(json_data)
-    return json_data
+
+    f.close()
+    f = open('message.txt','r')
+    fin = f.read()
+    print('*****')
+    print(fin)
+    f.close()
+    read_and_send(fin,new_number)
 
 
 # Function to retrieve messages from Inbox
@@ -78,12 +119,18 @@ def getMessages(apikey, inboxID):
     return (f.read(), f.code)
 
 
+# Function to send SMS
+def sendSMS(apikey, numbers,message,test=True):
+    params = {'apikey': apikey, 'numbers': numbers, 'message': message,'test':test}
+    f = urllib.request.urlopen('https://api.textlocal.in/send/?'
+                               + urllib.parse.urlencode(params))
+    return (f.read(), f.code)
+
 # Code to filter jobs based on message sent by user
-resp, code = getMessages('17dQ9VIqzsY-SaO7zL5u6LLm30oEhjNFLtsc4GssEa', 10)
-mess = json.loads(resp)
-for each_message in mess['messages']:
-    x = int(each_message['message'].split(',')[1])
-    get_nearest_jobs(x)
-
-
+if __name__ == '__main__':
+    resp, code = getMessages('17dQ9VIqzsY-SaO7zL5u6LLm30oEhjNFLtsc4GssEa', 10)
+    mess = json.loads(resp)
+    for each_message in mess['messages']:
+        x = int(each_message['message'].split(',')[2])
+        get_nearest_jobs(each_message['message'].split(',')[1],x,each_message['number'])
 
